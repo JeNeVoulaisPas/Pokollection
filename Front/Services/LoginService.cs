667c82ecdca04baa7d10a1531723cdb6d5a300ef
JeNeVoulaisPas <147522431+JeNeVoulaisPas@users.noreply.github.com>
2024-01-12 14:05:27 +0100

    Ajoutez des fichiers projet.

---
+++ Front/Services/LoginService.cs
@@ -0,0 +1,44 @@
+﻿using Front.Entities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+
+namespace Front.Services
+{
+    public class LoginService
+    {
+        private readonly HttpClient _httpClient;
+
+        public LoginService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new System.Uri("http://localhost:5000/");
+        }
+
+        public async Task<UserDTO?> AuthenticateUser(string username, string password)
+        {
+            UserLogin u = new UserLogin() { Name=username, Pass=password};
+            var res = await _httpClient.PostAsJsonAsync<UserLogin>("api/User/login", u);
+            if (res.IsSuccessStatusCode)
+            {
+                try
+                {
+                    UserDTO? udto = await res.Content.ReadFromJsonAsync<UserDTO>();
+                    return udto;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
+
---
+++ Front/Services/RegisterService.cs
@@ -0,0 +1,32 @@
+﻿using Front.Entities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+
+namespace Front.Services
+{
+    public class RegisterService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RegisterService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new System.Uri("http://localhost:5000/");
+        }
+
+        public async Task<bool> CreateUser(string email, string username, string password)
+        {
+            UserCreateModel u = new UserCreateModel() { Email = email, Name = username, Password = password };
+            var res = await _httpClient.PostAsJsonAsync<UserCreateModel>("api/User/register", u);
+
+            return res.IsSuccessStatusCode;
+
+
+        }
+    }
+}
+
+
