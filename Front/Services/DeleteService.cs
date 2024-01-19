ebc10a030f3cc319a9a5c88f0c6e157bbd857602
JeNeVoulaisPas <147522431+JeNeVoulaisPas@users.noreply.github.com>
2024-01-19 15:41:36 +0100

    Add pages

--- Front/Services/DeleteService.cs
+++ Front/Services/DeleteService.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using Front.Services;
+using Microsoft.AspNetCore.Components;
 
 namespace Front.Services
 {
@@ -21,12 +22,24 @@ namespace Front.Services
             var res = await _httpClient.DeleteAsync($"api/User");
             if (res.IsSuccessStatusCode)
             {
+                await Logout();
                 return true;
             }
             else
             {
+
                 return false;
             }
+        }
+
+        public async Task<bool> Logout()
+        {
+            if (_auth != null)
+            {
+                await _auth.Logout();
+                return true;
+            }
+            return false;
         }
     }
 }
